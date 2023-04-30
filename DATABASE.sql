7b48f34a17efb0d4a457931fb1e7728bfb45dcbe
OsorioCODE01 <Nicolas.Osor.Gal@gmail.com>
2023-04-30 12:46:03 -0500

    ADDING DB FILE

---
+++ DATABASE.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: bro0as1jn6iayumwpheg-mysql.services.clever-cloud.com:3306
+-- Generation Time: Apr 30, 2023 at 05:44 PM
+-- Server version: 8.0.15-5
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bro0as1jn6iayumwpheg`
+--
+CREATE DATABASE IF NOT EXISTS `bro0as1jn6iayumwpheg` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `bro0as1jn6iayumwpheg`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `log_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` int(25) UNSIGNED NOT NULL,
+  `accion` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `tabla` varchar(25) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `task_id` int(11) NOT NULL,
+  `user_id` int(25) UNSIGNED NOT NULL,
+  `task_name` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
+  `ShowTask` tinyint(1) NOT NULL DEFAULT '1',
+  `description` text COLLATE utf8mb4_general_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `finish` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `DNI` int(25) UNSIGNED NOT NULL,
+  `primer_nombre` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
+  `segundo_nombre` varchar(25) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `primer_apellido` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
+  `segundo_apellido` varchar(25) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `telefono` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `direccion` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `username` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(25) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`log_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`task_id`),
+  ADD KEY `tasks_ibfk_1` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`DNI`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `log_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `logs`
+--
+ALTER TABLE `logs`
+  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`DNI`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`DNI`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
